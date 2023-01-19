4ac021a7101c8e2c8a57e7fc9ca7cf550c0e2204
Annie Ehler <annie.ehler.4@gmail.com>
2023-01-19 11:55:00 -0800

    whoops

--- sleapyfaces/__about__.py
+++ sleapyfaces/__about__.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2022-present Annie Ehler <annie.ehler.4@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "1.2.0-beta2.dev8"
+__version__ = "1.2.0-beta2.dev10"
 __author__ = "Analetta (Annie) Ehler"
 __author_email__ = "annie.ehler.4@gmail.com"
 __license__ = "MIT"
