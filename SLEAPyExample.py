ffa9e207086d87f5a1746657be789caae7e2c960
Annie Ehler <annie.ehler.4@gmail.com>
2023-01-31 15:56:58 -0800

    Remove versioning from conda environment
     Last command done (1 command done):
        pick 4f79c287 Remove versioning from conda environment
     Changes to be committed:
    	modified:   .pyre/pyre.stderr
    	modified:   SLEAPyExample.ipynb
    	modified:   SLEAPyExample.py
    	modified:   environment.yml

--- SLEAPyExample.py
+++ SLEAPyExample.py
@@ -16,6 +16,12 @@
 from sleapyfaces.base import Experiment
 
 expr = Experiment(
+    name="SLEAPyExample",
+    base="/Volumes/specialk_cs/2p/raw/CSE011/20211105/",
+    ExperimentEventsFile=("*_events.csv", True),
+    ExperimentSetupFile=("*.json", True),
+    SLEAPFile=("*.h5", True),
+    VideoFile=("*.mp4", True),
 )
 
 # %%
