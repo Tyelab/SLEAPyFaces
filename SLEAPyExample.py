7ccf5caf9d714a3ef98dbb85013c284ad0f78bf5
Annie Ehler <annie.ehler.4@gmail.com>
2023-01-31 15:26:22 -0800

    add pip packages to environment

---
+++ SLEAPyExample.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: sleapyfaces
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from sleapyfaces import Experiment
+
+ecpr = Experiment(
+)
+
+# %%
